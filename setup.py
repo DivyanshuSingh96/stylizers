c8ccbef36c4d279208dd2c94d275d99029db12ef
DivyanshuSingh96 <79963506+DivyanshuSingh96@users.noreply.github.com>
2021-03-11 08:57:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md') as f:
     README = f.read()
 
 setuptools.setup(
-    name="stylizer",
+    name="stylizers",
     version="v0.0.1",
     author="Divyanshu Singh",
     author_email="divyanshu.singh144400@gmail.com",
     license="MIT",
     description='A Beautiful Library which will modify your Beautiful Name into an Extraordinary Styled Name.',
     long_description=README,
-    url='https://github.com/DivyanshuSingh96/stylizer',
+    url='https://github.com/DivyanshuSingh96/stylizers',
     packages=setuptools.find_packages(),
     python_requires=">=3.0",
     classifiers=[
